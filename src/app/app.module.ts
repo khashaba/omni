006c089aadd75abd7de11fd4a81918c6111ced2e
Ahmed Hassan <Ahmed.Hassan@Qcentriseg.com>
2020-06-22 13:28:06 +0200

    basic structure and sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GenericTableComponent } from './generic-table/generic-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenericTableComponent
   ],
   imports: [
     BrowserModule,
